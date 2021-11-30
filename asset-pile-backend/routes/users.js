db4f8b683b8496d5f3c26fb8e59cdfcf8dfb6f6f
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-11-30 20:46:01 +0530

    Added user model and updated user router

--- asset-pile-backend/routes/users.js
+++ asset-pile-backend/routes/users.js
@@ -1,26 +1,43 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var userRouter = express.Router();
 
-dishRouter.use(express.json());
+var Users = require('../models/user-model');
 
-dishRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
+userRouter.use(express.json());
+
+userRouter.route('/')
 .get((req,res,next) => {
-    res.end('Will send all the Users to you!');
+    Users.find({})
+    .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the user: ' + req.body.name);
+    Users.create(req.body)
+    .then((user) => {
+        console.log('User Created ', user);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(user);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /users');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all users');
+    Users.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
 });
 
 module.exports = userRouter;
