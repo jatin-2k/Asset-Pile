c6654a196f64f740dd084b7690c3c0cfaeb88d98
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-01-24 19:00:26 +0530

    Code Cleanup

--- frontend/src/components/asset-table.js
+++ frontend/src/components/asset-table.js
@@ -4,49 +4,7 @@ class AssetTable extends Component{
 
     render() {
         const data = this.props.data;
-        const name = (asset) => {
-            return (
-                <td>
-                <p className="mb-1 text-dark font-weight-medium">{asset.symbol}</p>
-                {/* <small className="font-weight-medium">{asset.fullname}</small> */}
-                </td>
-            );
-        };
-        const price = (assetPrice) => {
-            return <td className="font-weight-medium">${assetPrice}</td>;
-        };
-        const change = (assetchange) => {
-            const sign = (num) => {
-                return num >= 0
-                ? "text-success font-weight-medium"
-                : "text-danger font-weight-medium";
-            };
-            return <td className={sign(assetchange)}>{assetchange}</td>;
-        };
-        const button = () => {
-            return (
-                <td>
-                <button>Press</button>
-                </td>
-            );
-        };
-        const row = (asset) => {
-            return (
-                <tr>
-                {name(asset)}
-                {price(asset.historical[0].open)}
-                {change(asset.historical[0].change)}
-                {button()}
-                </tr>
-            );
-        };
-
-        const rows = (assets) => {
-            return assets.map((asset) => {
-                return row(asset);
-            });
-        };
-
+        
         const table = (assets) => {
             return (
                 <div className="col-md-12 grid-margin stretch-card">
@@ -71,8 +29,53 @@ class AssetTable extends Component{
                 </div>
             );
         };
+        
         return <div>{table(data)}</div>;
     }
 }
+
+//#region Helper Functions
+const name = (asset) => {
+    return (
+        <td>
+        <p className="mb-1 text-dark font-weight-medium">{asset.symbol}</p>
+        {/* <small className="font-weight-medium">{asset.fullname}</small> */}
+        </td>
+    );
+};
+const price = (assetPrice) => {
+    return <td className="font-weight-medium">${assetPrice}</td>;
+};
+const change = (assetchange) => {
+    const sign = (num) => {
+        return num >= 0
+        ? "text-success font-weight-medium"
+        : "text-danger font-weight-medium";
+    };
+    return <td className={sign(assetchange)}>{assetchange}</td>;
+};
+const button = () => {
+    return (
+        <td>
+        <button>Press</button>
+        </td>
+    );
+};
+const row = (asset) => {
+    return (
+        <tr>
+        {name(asset)}
+        {price(asset.historical[0].open)}
+        {change(asset.historical[0].change)}
+        {button()}
+        </tr>
+    );
+};
+const rows = (assets) => {
+    return assets.map((asset) => {
+        return row(asset);
+    });
+};
+//#endregion
 
 export default AssetTable;
